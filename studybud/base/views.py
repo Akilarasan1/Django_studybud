fb6643d0ee14eb456fe51d6410b12fc942d580d8
Akilarasan1 <akilarasan20@gmail.com>
2023-09-23 17:24:17 +0530

    delete error

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
 # Create your views here.
-
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all() 
@@ -11,5 +11,35 @@ def room(request, pk): #primary key
     return render(request,'base/room.html', {'room': room})
 
 def createRoom(request):
-    context = {}
-    return render(request,'base/room_form.html')
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        #print(request.POST)
+    context = {'form':form}
+    return render(request,'base/room_form.html', context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id =pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context ={'form':form}
+    return render(request, 'base/room_form.html',context)
+    
+    
+def deleteRoom(request, pk):
+    room = Room.objects.get(id= pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+        
+    return render(request, 'base/delete.html', {'obj':room})
